da285098ff88640c1025d9280c57d09a5fdd780d
augustovogt <augusto.vogt@gmail.com>
2023-03-17 10:52:54 -0300

    test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MONGO_URI } = container.resolve("config");
 const mongoose = require("mongoose");
 
 mongoose.set("useCreateIndex", true);
+console.log('prueba backend');
 
 mongoose.connect( MONGO_URI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true}).then(()=>{
     server.start();
